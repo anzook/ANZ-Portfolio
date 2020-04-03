3079d4688dc91ac0eac6aef9f2f2f48602d4ec99
anz213 <56008225+anzook@users.noreply.github.com>
2020-04-03 10:41:27 -0400

    draft logo landing page

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in Progress...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://anzook.github.io/Portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See more here
         </a>
       </header>
     </div>
