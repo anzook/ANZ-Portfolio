d230c93181528eb1917522a7f3c0d2e30b764a75
ANZook <alexnzook@gmail.com>
2020-05-15 11:15:02 -0400

    inital sraft state with logo

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <p>
+Alex N Zook        </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Work in Progress...
-        </p>
         <a
           className="App-link"
           href="https://anzook.github.io/Portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          See more here
+          See the Projects
         </a>
       </header>
     </div>
