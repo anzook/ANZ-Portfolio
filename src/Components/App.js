57434d6ba3088a5d0db72496b67996a627f812ec
ANZook <alexnzook@gmail.com>
2020-05-21 01:42:17 -0400

    drafted left hand nav

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Pagination } from "react-bootstrap";
+import { Pagination, Container, Row, Col } from "react-bootstrap";
 
 import ReactPageScroller from "./ReactPageScrollerSRC";
 import FirstComponent from "./FirstComponent";
@@ -11,18 +11,18 @@ import './App.css';
 
 export default function Portfolio(props) {
 const [currentPage, setCurrentPage] = useState(0)
-  
-const PageHeaders = ["Landing", "Welcome", "Projects", "About", "Contact"]
+let active = currentPage +1;
   const handlePageChange = number => {
    setCurrentPage( number ); // set currentPage number, to reset it from the previous selected.
   };
 
   const getPagesNumbers = () => {
     const pageNumbers = [];
+    const PageHeaders = ["Landing", "Welcome", "Projects", "About", "Contact"]
     
     for (let i = 1; i <= 5; i++) {
       pageNumbers.push(
-        <Pagination.Item key={i} eventKey={i - 1} onClick={ () => handlePageChange(i-1)}>
+        <Pagination.Item key={i} active={i === active} onClick={ () => handlePageChange(i-1)} className="pagination-item">
           {PageHeaders[i-1]}
         </Pagination.Item>,
       );
@@ -35,18 +35,31 @@ const PageHeaders = ["Landing", "Welcome", "Projects", "About", "Contact"]
 
     return (
       <React.Fragment>
-        <ReactPageScroller
+                    <Container fluid>
+  <Row className="justify-content-md-center">
+
+    <Col className ="scrolling-content"  md="auto">
+    <ReactPageScroller
           pageOnChange={handlePageChange}
           customPageNumber={currentPage} >
+
           <FirstComponent />
           <SecondComponent />
           <ThirdComponent />
           <FourthComponent />
           <FifthComponent />
         </ReactPageScroller>
-        <Pagination className="pagination-additional-class" size="lg">
+     </Col>
+     <Col >
+  <Pagination className="pagination-additional-class">
+  {/* <Pagination.Prev /> */}
           {pagesNumbers}
+          {/* <Pagination.Next /> */}
         </Pagination>
+    </Col>
+  </Row>
+</Container>
+        
       </React.Fragment>
     );
   }
--- src/Components/FirstComponent.js
+++ src/Components/FirstComponent.js
@@ -1,12 +1,36 @@
 import React from "react";
 import logo from './Logo/logo.svg';
-
+import { Container, Row, Col, Media } from 'react-bootstrap';
 
 export default () => {
   return (
-    <div className="component first-component">
-      <h2>Landing Page</h2>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
+<div className="component first-component">
+<Container fluid>
+  <Row className="justify-content-md-center">
+    <Col className =""  md="auto">
+    <Media>
+    <img src={logo} className="align-self-center App-logo logo-main" alt="logo" />
+    <Media.Body>
+      <h5>Media Heading</h5>
+      <p>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+        ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+        tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+        fringilla. Donec lacinia congue felis in faucibus.
+      </p>
+
+      <p>
+        Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
+        leo. Cum sociis natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus.
+      </p>
+    </Media.Body>
+    </Media>
+     </Col>
+   
+  </Row>
+</Container>
+</div>
+
   );
 };
